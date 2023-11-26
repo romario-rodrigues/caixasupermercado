9d56e175c460afc629706c2d064af7a492017c14
Romario Rodrigues <romariobdc2011@gmail.com>
2023-11-26 17:49:36 -0300

    Faltar concluir o codigo de venda

--- caixa.py
+++ caixa.py
@@ -12,15 +12,23 @@ total = []
 
 
 while True:
+   
    produto = input("Digite o nome do produto: ")
+   
+   
    if produto == "sair":
-      for itens in carrinho:
-         print(itens)
       break
-   valor = produtoevalor.get(produto)
-   carrinho.extend([produto, valor])
-   total.append(valor)
+   
+   else:
+    quantidade = int(input("Digite a quantidade: "))
+    valor = produtoevalor.get(produto)
+    carrinho.extend([produto, valor])
+    total.append(quantidade * valor)
+    
 
-   
-   print(f"TOTAL: R$ {sum(total):.2f}")
-   
+    
+    print(f"TOTAL: R$ {sum(total):.2f}")
+
+for compra in carrinho:
+  print(compra)
+print(f"Total a Pagar R$ : {sum(total):.2f}")
