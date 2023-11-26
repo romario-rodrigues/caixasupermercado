d8831a7447833b58c7ebf7c9eae31f157919c091
Romario Rodrigues <romariobdc2011@gmail.com>
2023-11-26 16:20:08 -0300

    Criado a lista que coleta os valores dos produtos

--- caixa.py
+++ caixa.py
@@ -1,1 +1,17 @@
-print("Hello, Word!")
+# Nesse projeto sera criado um simulador de caixa de supermercado simples sem DB, usaremos apenas python simples.
+
+produtoevalor = {"arroz": 5.50, "feijao": 4.50, "oleo": 8.00}
+
+produto = ""
+
+listaproduto = []
+
+valor = 0
+
+
+while produtoevalor != "sair":
+   produto = input("Digite o nome do produto: ")
+   valor = produtoevalor.get(produto)
+
+   print (f"R$ {valor:.2f}")
+
